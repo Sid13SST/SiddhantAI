0c5e714699f9c533a30c902ff962d16389ae5265
Siddhant <siddhant@example.com>
2026-06-05 18:57:43 +0530

    feat: Add init module and models exports to app/models directory

---
+++ app/models/__init__.py
@@ -0,0 +1,14 @@
+from .document import (
+    SourceType,
+    BaseMetadata,
+    ResumeMetadata,
+    GitHubRepoMetadata,
+    GitHubReadmeMetadata,
+    GitHubCommitMetadata,
+    GitHubCodeMetadata,
+    IngestedDocument,
+    DocumentChunk,
+    SearchResult,
+    ProjectSummary,
+    PersonaProfile,
+)
